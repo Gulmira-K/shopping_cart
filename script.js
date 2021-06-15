4cf9b90e1eefd123400a3c6d8c1e4bf7d0049f3f
Gulmira-K <73161951+Gulmira-K@users.noreply.github.com>
2021-06-15 18:15:44 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,76 @@
+let itemsContainer = document.querySelector('.shop--items')
+let cartContainer = document.querySelector('.shop--cart')
+
+let itemsList = [
+  {
+    name: 'meat',
+    image: './media/meat.png',
+    price: 10
+  },
+  {
+    name: 'milk',
+    image: './media/milk.png',
+    price: 1.5
+  },
+  {
+    name: 'bread',
+    image: './media/bread.png',
+    price: 3
+  },
+  {
+    name: 'broccoli',
+    image: './media/brocoli.png',
+    price: 2
+  },
+  {
+    name: 'cereals',
+    image: './media/cereals.png',
+    price: 1.5
+  },
+  {
+    name: 'cheese',
+    image: './media/cheese.png',
+    price: 4
+  },
+  {
+    name: 'eggs',
+    image: './media/eggs.png',
+    price: 4
+  },
+  {
+    name: 'jam',
+    image: './media/jam.png',
+    price: 4
+  },
+  {
+    name: 'lettuce',
+    image: './media/lettuce.png',
+    price: 4
+  },
+  {
+    name: 'pasta',
+    image: './media/pasta.png',
+    price: 4
+  },
+  {
+    name: 'sausages',
+    image: './media/sausages.png',
+    price: 4
+  },
+  {
+    name: 'tomatos',
+    image: './media/tomate.png',
+    price: 4
+  },
+]
+
+let item = itemsList.map(item => {
+  return (
+    `<li class='shop--item'>
+      <img src=${item.image} />
+      <p>${item.price}€</p>
+    </li>`
+  )
+})
+
+itemsContainer.innerHTML = item.join('')
